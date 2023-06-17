95b0029f3f183a7aee5fc20afb058baa8db88247
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-17 21:39:11 +0530

    configured basic setup for react using parcel

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { ReactDOM } from "react";
+
+const App = () =>{
+    return (
+        <div>
+            This is react-app
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"));
